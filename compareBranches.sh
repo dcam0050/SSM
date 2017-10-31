9c05b65ad0c0568a82c1e5068fc8a9b1e4e27de5
dcam0050 <daniel.james.camilleri.13@gmail.com>
2017-10-31 00:37:51 +0000

    compare update

--- compareBranches.sh
+++ compareBranches.sh
@@ -5,19 +5,19 @@ branch_name="(unnamed branch)"     # detached HEAD
 branch_name=${branch_name##refs/heads/}
 echo "Current branch", $branch_name
 
-# mkdir -p ../comparisonDir/$branch_name && cp -r * ../comparisonDir/$branch_name
+mkdir -p ../comparisonDir/$branch_name && cp -r * ../comparisonDir/$branch_name
 
-# branches=()
-# eval "$(git for-each-ref --shell --format='branches+=(%(refname))' refs/heads/)"
+branches=()
+eval "$(git for-each-ref --shell --format='branches+=(%(refname))' refs/heads/)"
 
-# for branch in "${branches[@]}"; do
-#     b=${branch##refs/heads/}
-#     if [ $b != $branch_name ]
-#     then
-#     	git checkout $b 
-#     	meld . ../comparisonDir/$branch_name
-#     	git add --all
-#     	git commit -m $1
-#     fi
-# done
-# git checkout $branch_name
+for branch in "${branches[@]}"; do
+    b=${branch##refs/heads/}
+    if [ $b != $branch_name ]
+    then
+    	git checkout $b 
+    	meld . ../comparisonDir/$branch_name
+    	git add --all
+    	git commit -m $1
+    fi
+done
+git checkout $branch_name
