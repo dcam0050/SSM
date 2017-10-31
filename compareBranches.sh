ab55f791d8b8a649e63361e12bd3a0b3e83674fd
dcam0050 <daniel.james.camilleri.13@gmail.com>
2017-10-31 00:31:39 +0000

    exec compareBranches

--- compareBranches.sh
+++ compareBranches.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-
+echo $1
 branch_name="$(git symbolic-ref HEAD 2>/dev/null)" ||
 branch_name="(unnamed branch)"     # detached HEAD
 branch_name=${branch_name##refs/heads/}
 echo "Current branch", $branch_name
 
-mkdir -p ../comparisonDir/$branch_name && cp -r * ../comparisonDir/$branch_name
+# mkdir -p ../comparisonDir/$branch_name && cp -r * ../comparisonDir/$branch_name
 
-branches=()
-eval "$(git for-each-ref --shell --format='branches+=(%(refname))' refs/heads/)"
+# branches=()
+# eval "$(git for-each-ref --shell --format='branches+=(%(refname))' refs/heads/)"
 
-for branch in "${branches[@]}"; do
-    b=${branch##refs/heads/}
-    if [ $b != $branch_name ]
-    then
-    	git checkout $b 
-    	meld . ../comparisonDir/$branch_name
-    	git add --all
-    	git commit -m
-    fi
-done
-git checkout $branch_name
+# for branch in "${branches[@]}"; do
+#     b=${branch##refs/heads/}
+#     if [ $b != $branch_name ]
+#     then
+#     	git checkout $b 
+#     	meld . ../comparisonDir/$branch_name
+#     	git add --all
+#     	git commit -m $1
+#     fi
+# done
+# git checkout $branch_name
