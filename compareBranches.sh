af3db4d2b15a399e236c2aabe29afae44fff03ea
dcam0050 <daniel.james.camilleri.13@gmail.com>
2017-10-31 01:17:57 +0000

    "Fixed library referencing. COnverted samOptimiser to use logging"

---
+++ compareBranches.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+branch_name="$(git symbolic-ref HEAD 2>/dev/null)" ||
+branch_name="(unnamed branch)"     # detached HEAD
+branch_name=${branch_name##refs/heads/}
+echo "Current branch", $branch_name
+
+mkdir -p ../comparisonDir/$branch_name && cp -r * ../comparisonDir/$branch_name
+
+branches=()
+eval "$(git for-each-ref --shell --format='branches+=(%(refname))' refs/heads/)"
+
+for branch in "${branches[@]}"; do
+    b=${branch##refs/heads/}
+    if [ $b != $branch_name ]
+    then
+    	git checkout $b 
+    	meld . ../comparisonDir/$branch_name
+    	git add --all
+    	git commit -m '"'$1'"'
+    fi
+done
+git checkout $branch_name
